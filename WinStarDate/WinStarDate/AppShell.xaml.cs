607554651d2dcd45c62bde57d589d77fa2a8e218
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-08-11 20:06:55 +0200

    Aggiungere i file di progetto.

---
+++ WinStarDate/WinStarDate/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using WinStarDate.ViewModels;
+using WinStarDate.Views;
+using Xamarin.Forms;
+
+namespace WinStarDate
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ WinStarDate/WinStarDate/Views/AppPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WinStarDate.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppPage : ContentPage
+    {
+        public AppPage()
+        {
+            InitializeComponent();
+            data.Date = DateTime.Parse(Preferences.Get("Data", "1968-09-08"));
+        }
+        private void calcola_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(data.Date.Date.ToUniversalTime().ToString("dd")) + 1;
+            String s = "" + i;
+            if (s.Length == 1)
+                s = "0" + s;
+            risultato.Text = "Selected Stellar Date is: "+data.Date.Date.ToUniversalTime().ToString("yy") + data.Date.Date.ToUniversalTime().ToString("MM") + "." + s;
+            Preferences.Set("Data", data.Date.ToString());
+
+        }
+    }
+}
